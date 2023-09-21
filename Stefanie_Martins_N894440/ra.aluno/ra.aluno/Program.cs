ea9f35d1061222097c881edb67934be3e9bdbf5f
stefanie-martins <132002364+stefanie-martins@users.noreply.github.com>
2023-09-20 23:24:49 -0300

    Add files via upload

---
+++ Stefanie_Martins_N894440/ra.aluno/ra.aluno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Aluno
+{
+    public virtual string GetNome()
+    {
+        return "";
+    }
+}
+
+class AlunaStefanie : Aluno
+{
+    public override string GetNome()
+    {
+        return "Stefanie Martins F.";
+    }
+
+    public string GetRA()
+    {
+        return "n89444";
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno stefanie = new AlunaStefanie();
+        Console.WriteLine("RA: " + ((AlunaStefanie)stefanie).GetRA());
+        Console.WriteLine("Nome: " + stefanie.GetNome());
+    }
+}
